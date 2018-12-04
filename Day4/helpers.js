4485fbf3cc6b48633c3d33242346f9ec2567542c
Uriel Hayon <uhayon@overactiveinc.com>
2018-12-04 11:14:52 -0300

    Part 1

--- Day4/helpers.js
+++ Day4/helpers.js
@@ -1,5 +1,16 @@
 const getLogTimeStamp = log => {
-  return new Date(log.substr(log.indexOf('[') + 1, log.indexOf(']') - 1))
+  return new Date(getTimestampString(log));
+}
+
+const getTimestampString = log => {
+  return log.substr(log.indexOf('[') + 1, log.indexOf(']') - 1);
+}
+
+const getLogMinutes = log => {
+  const timestampString = getTimestampString(log);
+  const [date, time] = timestampString.split(' ');
+  const [hour, minutes] = time.split(':');
+  return minutes;
 }
 
 const orderLogs = logs => {
@@ -16,7 +27,42 @@ const orderLogs = logs => {
   });
 }
 
+const getGuardId = log => {
+  const logStartingFromId = log.substr(log.indexOf('#') + 1);
+  return logStartingFromId.substr(0, logStartingFromId.indexOf(' '));
+}
+
+const buildGuardsHistory = logs => {
+  const guardsHistory = {};
+  let guardId;
+  let asleepFrom;
+  logs.forEach(log => {
+    const upperCaseLog = log.toUpperCase();
+    if (upperCaseLog.match(/BEGINS SHIFT/) !== null) {
+      guardId = getGuardId(log);
+    }
+
+    if (upperCaseLog.match(/FALLS ASLEEP/) !== null) {
+      asleepFrom = Number(getLogMinutes(log));
+    }
+
+    if (upperCaseLog.match(/WAKES UP/) !== null) {
+      let asleepTo = Number(getLogMinutes(log));
+      guardsHistory[guardId] = (!guardsHistory[guardId]) ? {} : guardsHistory[guardId];
+
+      for (let i = asleepFrom; i < asleepTo; i++) {
+        guardsHistory[guardId][i] = (!guardsHistory[guardId][i]) ? 1 : (guardsHistory[guardId][i] + 1);
+      }
+    }
+  });
+  return guardsHistory;
+}
+
 module.exports = {
   getLogTimeStamp,
-  orderLogs
+  orderLogs,
+  getTimestampString,
+  getLogMinutes,
+  getGuardId,
+  buildGuardsHistory
 }
--- Day4/part1.js
+++ Day4/part1.js
@@ -1,4 +1,43 @@
 const { inputArray } = require('./input');
-const { orderLogs } = require('./helpers');
+const { orderLogs, buildGuardsHistory } = require('./helpers');
 
-console.log(orderLogs(inputArray));
+const guardsHistory = buildGuardsHistory(orderLogs(inputArray));
+console.log(guardsHistory);
+
+const getMostSleepyGuard = () => {
+  const mostSleepyGuard = {
+      id: -1,
+      totalAsleepMinutes: -1,
+      mostAsleepMinute: {
+        minute: -1,
+        timesAsleep: -1
+      }
+  };
+
+  for (guard in guardsHistory) {
+    let totalAsleepMinutes = 0;
+    let mostAsleepMinute = {
+      minute: -1,
+      timesAsleep: -1
+    };
+
+    for (minute in guardsHistory[guard]) {
+      totalAsleepMinutes += guardsHistory[guard][minute];
+      if (guardsHistory[guard][minute] > mostAsleepMinute.timesAsleep) {
+        mostAsleepMinute.minute = minute;
+        mostAsleepMinute.timesAsleep = guardsHistory[guard][minute];
+      }
+    }
+
+    if (totalAsleepMinutes > mostSleepyGuard.totalAsleepMinutes) {
+      mostSleepyGuard.id = guard;
+      mostSleepyGuard.totalAsleepMinutes = totalAsleepMinutes;
+      mostSleepyGuard.mostAsleepMinute = mostAsleepMinute;
+    }
+  }
+
+  return mostSleepyGuard;
+}
+
+const mostSleepyGuard = getMostSleepyGuard();
+console.log(`${mostSleepyGuard.id} * ${mostSleepyGuard.mostAsleepMinute.minute} = ${mostSleepyGuard.id * mostSleepyGuard.mostAsleepMinute.minute}`);
